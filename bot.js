fdaab5adc4d6488ede743050f2b10b40416ed388
RenegadeB5 <rocketmanB5@gmail.com>
2019-03-04 13:16:02 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ client.on('ready', () => {
 
 client.on ('message', message => {
 	if (message.author.id === '365975655608745985' && message.channel.id === '547950225327783976') {
+		console.log(message.content);
 		let pokemon = message.embeds.map(r => r.fields.map(r => r.name))[0];
 		let uri = "mongodb+srv://RenegadeB5:sapphyres@cluster0.mongodb.net/pokemon"
  		console.log(pokemon);
