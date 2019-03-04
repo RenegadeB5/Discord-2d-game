9871280ac8f29ce27585a3a7fa4ebd23c483281e
RenegadeB5 <rocketmanB5@gmail.com>
2019-03-04 11:39:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,19 @@ client.on('ready', () => {
 client.on ('message', message => {
 	if (message.author.id === '365975655608745985' && message.channel.id === '547950225327783976') {
 		let pokemon = message.embeds.map(r => r.fields.map(r => r.name))[0];
+		let uri = "mongodb+srv://RenegadeB5:sapphyres@cluster0.mongodb.net/pokemon"
  		console.log(pokemon);
 		console.log(pokemon[5]);
+		global.MongoClient.connect(uri, function(err, client) {
+					if (err) {
+						console.error('An error occurred connecting to MongoDB: ', err);
+					}
+					else {
+						const collection = client.db("pokedex").collection("pokemon");
+						collection.insertOne({ name: "Company Inc"}, function(err, res) {
+						client.close();
+					}
+				});
 	}
 });    
 
