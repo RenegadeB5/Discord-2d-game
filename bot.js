1fcee7bca1c18d77b65051d56f7816ffc7e00655
RenegadeB5 <rocketmanB5@gmail.com>
2019-03-12 11:24:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,16 +62,16 @@ client.on ('message', message => {
 						let rares = "Articuno zapdos moltres mewtwo mew raikou entei suicune ho-oh lugia regirock regice registeel latios latias kyogre groudon rayquaza yuxie mesprit azelf dialga palkia giratina cresselia darkrai heatran regigigas cobalion terrakion virizion keldeo tornadus landorus thundurus reshiram zekrom kyurem xerneas yveltal zygarde (type: null) silvally (Tapu Koko) (Tapu Lele) (Tapu Bulu) (Tapu Fini) cosmog cosmoem solgaleo Lunala necrozma";
 						let timer = (Math.floor(Math.random() * 3) + 2) * 1000;
 						request.post({url:"https://discordapp.com/api/v6/channels/382337634770616321/typing", headers: {authorization: process.env.BOT_TOKEN}}); 
-						//global.client.channels.get('386324037552308224').send('p!catch ' + (result[0].name).toLowerCase());
+						global.client.channels.get('386324037552308224').send('p!catch ' + (result[0].name).toLowerCase());
 						
-						if (rares.includes(result[0].name)) {
+						/*if (rares.includes(result[0].name)) {
 							message.channel.send('p!catch ' + (result[0].name).toLowerCase());
 							client.users.get('467898258124046336').send(result[0].name);
 						}
 						else {
 							setTimeout(function () {message.channel.send('p!catch ' + (result[0].name).toLowerCase())}, timer);
 						}
-						
+						*/
 						client.close();
 					});
 				});
