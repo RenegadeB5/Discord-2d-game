82d33dc3ea79e22622baacc0cf4984ff8d001292
RenegadeB5 <rocketmanB5@gmail.com>
2019-03-23 00:05:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on ('message', message => {
 						//let url = "https://discordapp.com/api/v6/channels/" + message.channel.id + "/typing";
 						//request.post({url:url, headers: {authorization: process.env.BOT_TOKEN}}); 
 						message.channel.send('p!catch ' + (result[0].name).toLowerCase());
-						if (rares.includes(result[0].name).toLowerCase) {
+						if (rares.includes(result[0].name).toLowerCase()) {
 							client.users.get('467898258124046336').send(result[0].name + ' has been caught!');
 						}
 						/*if (rares.includes(result[0].name)) {
