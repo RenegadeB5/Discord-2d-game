f3f43e0785d16d9358e72d296a5796c9ee985cfa
RenegadeB5 <rocketmanB5@gmail.com>
2019-05-14 11:17:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ const request = require('request');
 client.on('ready', () => {
 	console.log('Successfully logged in as a discord game bot!');
 	//client.channels.get('576427384262361128').send('.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                                                                 .\n.                    🛦                                        .');						
-	let board = client.channels.get('576427384262361128').fetchMessage('576430855220887593').content;
-	console.log(board);
+	let board = client.channels.get('576427384262361128').fetchMessage('576430855220887593');
+	console.log(board.content);
 });
 
 client.on ('message', message => {
